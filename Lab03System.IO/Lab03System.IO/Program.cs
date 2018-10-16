d7b1fc45eb8f3936b2a7d022be07c1f98553febe
thejamilah <jamilahmcw@gmail.com>
2018-10-15 19:29:14 -0700

    push to branch...holy red lines batman

--- Lab03System.IO/Lab03System.IO/Program.cs
+++ Lab03System.IO/Lab03System.IO/Program.cs
@@ -3,109 +3,124 @@ using System.IO;
 
 namespace Lab03System.IO
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-
-            string path = "../../../wordguess.txt";
             Console.WriteLine("Shall we play a game?");
-            //using CreateFile creates the file in relative to the execution
-            CreateFile(path);
-            ReadFile(path);
-            UpdateFile(path);
-            DeleteLinesFromFile(path);
-
+            Menu();
         }
 
-        /// <summary>
-        /// Create path to external file, use try catch to prevent memory leaks 
-        /// to close out connections put in resource and using StreamWriter to write to fie
-        /// </summary>
-        /// <param name="path">passing path location</param>
-        static void CreateFile(string path)
+        public static void Menu()
         {
-            
-            try
+            Console.WriteLine("1. Play Game? A strange game. The only winning move is not to play.\n" +
+                "2. Admin Menu\n" +
+                "3. Exit\n");
+            int userSelection = int.Parse(Console.ReadLine());
+        }
+
+
+        
+
+        string path = "../../../wordguess.txt";
+        //using CreateFile creates the file in relative to the execution
+        CreateFile(path);
+        ReadFile(path);
+        UpdateFile(path);
+        DeleteLinesFromFile(path);
+
+        
+
+    }
+
+    /// <summary>
+    /// Create path to external file, use try catch to prevent memory leaks 
+    /// to close out connections put in resource and using StreamWriter to write to fie
+    /// </summary>
+    /// <param name="path">passing path location</param>
+    static void CreateFile(string path)
+    {
+
+        try
+        {
+            using (StreamWriter sw = new StreamWriter(path))
             {
-                using (StreamWriter sw = new StreamWriter(path))
+                try
                 {
-                    try
-                    {
-                        sw.Write("");
-                    }
-                    catch (Exception)
-                    {
-                        throw;
-                    }
-                    finally
-                    {
-                        sw.Close();
-                    }
-                    
+                    sw.Write("");
                 }
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-            finally
-            {
-                //close the file
-
-            }
-        }
-
-        static void ReadFile(string path)
-        {
-            try
-            {
-                using (StreamReader sr = File.OpenText(path))
+                catch (Exception)
                 {
-                    string[] guessWords = File.ReadAllLines(path);
-
-                    //while ((guessWords = sr.guessWords()) != null)
-                    //{
-                    //    Console.WriteLine(guessWords);
-                    //}
+                    throw;
                 }
-            }
-            catch (Exception)
-            {
-
-                throw;
-            } 
-        }
-
-        static void UpdateFile(string path)
-        {
-            try
-            {
-                using (StreamWriter sw = File.AppendText(path))
+                finally
                 {
-                    sw.WriteLine("");
+                    sw.Close();
                 }
-            }
-            catch (Exception)
-            {
 
-                throw;
             }
-
         }
-
-        static void DeleteLinesFromFile(string path)
+        catch (Exception)
         {
-            try
-            {
-                string[] currentGuessWords = ReadFile(path);
-            }
-            catch (Exception)
-            {
 
-                throw;
-            }
+            throw;
+        }
+        finally
+        {
+            //close the file
+
         }
     }
+
+    static void ReadFile(string path)
+    {
+        try
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string[] guessWords = File.ReadAllLines(path);
+
+                //while ((guessWords = sr.guessWords()) != null)
+                //{
+                //    Console.WriteLine(guessWords);
+                //}
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+    static void UpdateFile(string path)
+    {
+        try
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("");
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+
+    }
+
+    static void DeleteLinesFromFile(string path)
+    {
+        try
+        {
+            string[] currentGuessWords = ReadFile(path);
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
 }
